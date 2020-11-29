f0a945647af6a2d8e5c2f5c531a61e72c9c8ddaf
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-11-29 02:19:34 -0300

    Validações cursos

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CursosRoutingModule } from './cursos-routing.module';
+import { CursosFormComponent } from './cursos-form/cursos-form.component';
 import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
 
-
 @NgModule({
-  declarations: [CursosListaComponent],
-  imports: [
-    CommonModule,
-    CursosRoutingModule
-  ]
+  declarations: [CursosListaComponent, CursosFormComponent],
+  imports: [CommonModule, CursosRoutingModule, ReactiveFormsModule],
 })
-export class CursosModule { }
+export class CursosModule {}
